caf8c18dedb2117dd45427f3287d4912482abfd5
kinuthier <kkinuthia19@gmail.com>
2022-10-18 16:06:42 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { Toaster } from 'react-hot-toast'
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+    <Component {...pageProps} />
+    <Toaster />
+    </ThirdwebProvider>
+  )
+}
+
+export default MyApp
